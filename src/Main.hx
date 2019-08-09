93bf04dfe0aabe4c3e8f08df9843597e3cb11c32
John Dittmar <john.dittmar@gmail.com>
2019-08-08 21:48:50 -0500

    skellyboi

--- src/Main.hx
+++ src/Main.hx
@@ -1,11 +1,38 @@
+import h3d.scene.Object;
+import h3d.pass.DefaultShadowMap;
 import hxd.Key;
+import h3d.scene.fwd.DirLight;
 
 class Main extends hxd.App {
-	var bmp:h2d.Bitmap;
+	var cache : h3d.prim.ModelCache;
+	var skellyboi : Object;
 
 	override function init() {
-		bmp = new h2d.Bitmap(hxd.Res.bugs.toTile(), s2d);
-		hxd.Window.getInstance().addEventTarget(onEvent);		
+		cache = new h3d.prim.ModelCache();
+
+		var skellyboi = cache.loadModel(hxd.Res.models.Skeleton);
+		skellyboi.scale(0.1);
+		s3d.addChild(skellyboi);
+		s3d.camera.pos.set( -3, -5, 3);
+		s3d.camera.target.z += 1;
+
+		skellyboi.playAnimation(cache.loadAnimation(hxd.Res.models.Skeleton));
+		// add lights and setup materials
+		var dir = new DirLight(new h3d.Vector( -1, 3, -10), s3d);
+		for( m in skellyboi.getMaterials() ) {
+			var t = m.mainPass.getShader(h3d.shader.Texture);
+			if( t != null ) t.killAlpha = true;
+			m.mainPass.culling = None;
+			m.getPass("shadow").culling = None;
+		}
+		s3d.lightSystem.ambientLight.set(0.4, 0.4, 0.4);
+
+		var shadow = s3d.renderer.getPass(h3d.pass.DefaultShadowMap);
+		shadow.power = 20;
+		shadow.color.setColor(0x301030);
+		dir.enableSpecular = true;
+
+		new h3d.scene.CameraController(s3d).loadFromCamera();
 	}
 
 	function onEvent(event:hxd.Event) {
@@ -21,7 +48,7 @@ class Main extends hxd.App {
 	// on each frame
 	override function update(dt:Float) {
 		if (Key.isDown(Key.W)) {
-			// Placeh-holder
+			// shadow.power += 1;
 		}
 	}
 
